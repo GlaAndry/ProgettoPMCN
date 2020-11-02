0da1256e7a5eb48e7cae7bbb3c8ba6c6b5170d09
leopetr95 <leonardo_petraglia@hotmail.it>
2020-11-02 21:54:41 +0100

    delete prova

--- C/Codice/basic.c
+++ C/Codice/basic.c
@@ -5,12 +5,6 @@
 #include "basic.h"
 #include "parser.h"
 
-double prova(int i){
-
-return i;
-
-}
-
 double*alloc_array_double(int lenght){
     if(lenght<=0){
         handle_error_with_exit("error in parameter\n");
